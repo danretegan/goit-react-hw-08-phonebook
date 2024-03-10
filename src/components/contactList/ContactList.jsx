78f265d3af78c1307e3833e3f16cb12ea1cd6e43
Dan Retegan <danretegan@yahoo.com>
2024-03-10 10:28:52 +0200

    Update ContactList.jsx

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -18,6 +18,7 @@ import {
   ErrorMessage as ErrorMsg,
   ContactListContainer,
 } from './ContactList.styled';
+import { Loader } from '../loader/Loader';
 
 const ContactList = () => {
   const dispatch = useDispatch();
@@ -35,6 +36,7 @@ const ContactList = () => {
 
   return (
     <ContactListContainer>
+      {isLoading && <Loader />}{' '}
       {!isLoading && !error && (
         <ListContainer>
           <ListLabel>
