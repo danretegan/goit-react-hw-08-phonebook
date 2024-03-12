d8358585cdb41e326d951b5268961c335287b568
Dan Retegan <danretegan@yahoo.com>
2024-03-12 09:42:29 +0200

    add email info in UserMenu

--- src/components/userMenu/UserMenu.jsx
+++ src/components/userMenu/UserMenu.jsx
@@ -1,23 +1,24 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/auth-operations';
 import { useAuth } from '../../hooks';
-import { Container, Name } from './UserMenu.styled';
+import { Container, Email, Name, Section } from './UserMenu.styled';
 import Button from '../Button';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
-  const handleLogout = () => {
-    dispatch(logOut());
-  };
+  const handleLogout = () => dispatch(logOut());
 
   return (
     <Container>
-      <Name>Welcome {user.name}</Name>
-      <Button type="button" action={handleLogout}>
-        Logout
-      </Button>
+      <Section>
+        <Name>Welcome {user.name}</Name>
+        <Button type="button" action={handleLogout}>
+          Logout
+        </Button>
+      </Section>
+      <Email>{user.email}</Email>
     </Container>
   );
 }
--- src/components/userMenu/UserMenu.styled.js
+++ src/components/userMenu/UserMenu.styled.js
@@ -2,10 +2,19 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+export const Section = styled.div`
+  display: flex;
   align-items: center;
 `;
 
 export const Name = styled.span`
   font-weight: 700;
   margin-right: 12px;
+`;
+
+export const Email = styled.span`
+  margin-left: auto;
 `;
