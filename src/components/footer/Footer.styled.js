c6c5e190068c9fc2b205ee70abc2b47ed6f915e5
Dan Retegan <danretegan@yahoo.com>
2024-03-13 12:25:12 +0200

    Update Footer.styled.js

--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -2,23 +2,36 @@ import { BsGithub } from 'react-icons/bs';
 import styled from 'styled-components';
 
 export const FooterStyles = styled.footer`
+  width: 600px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  border-top: solid 1px #2a363b;
   @media only screen and (max-width: 600px) {
     width: 320px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0;
   }
-  width: 600px;
-  border-top: solid 1px #2a363b;
 `;
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 600px) {
+    margin-bottom: 16px;
+  }
 `;
 
 export const Icon = styled(BsGithub)`
+  height: 20px;
   margin-left: 10px;
   width: 20px;
-  height: 20px;
   transition: fill 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
 
   &:hover,
